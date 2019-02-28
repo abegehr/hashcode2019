3f357db457bbe7ea28d4685de869ed5d735715dd
Anton Begehr <a.begehr@gmail.com>
2019-02-28 20:16:23 +0100

    add read and write example

--- example_a.js
+++ example_a.js
@@ -1,3 +1,4 @@
+/*
 var pics_h = [
   {id: 0, tags: ["cat", "beach", "sun"]},
   {id: 3, tags: ["garden", "cat"]}
@@ -7,3 +8,25 @@ var pics_v = [
   {id: 1, tags: ["selfie", "smile"]},
   {id: 2, tags: ["garden", "selfie"]}
 ];
+*/
+
+const read = require("./parser").read;
+const write = require("./output").write_slides;
+
+
+// read example
+
+var data = read("A");
+
+console.log(data);
+
+
+// write example
+
+var slides = [
+  [0, 1],
+  [4],
+  [3, 5]
+];
+
+write("out_ex.txt", slides);
