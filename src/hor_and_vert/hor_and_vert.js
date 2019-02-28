7e4ca148a83df1f3dd0126fbc8bd182a8e2b56e4
tguelenman <tguelenman@zedat.fu-berlin.de>
2019-02-28 22:23:30 +0100

    Vs strategy

--- src/hor_and_vert/hor_and_vert.js
+++ src/hor_and_vert/hor_and_vert.js
@@ -4,7 +4,7 @@ const write = require("../output").write_slides;
 
 
 // change to A, B, C, D, E
-const file = "E";
+const file = "B";
 
 // read data
 var data = read(file);
@@ -49,15 +49,64 @@ const combinePics = (pic1, pic2) => {
   }
 
 const pairAllPicsV = () => {
-  var allPairs = []
-  for (var i = 0; i < pics_v.length; i=i+2){
+
+  var allPairs = pics_v
+  allPairs.sort(function(a, b){
+    if(a["tags"].length < b["tags"].length) return -1;
+    if(a["tags"].length > b["tags"].length) return 1;
+    return 0;
+  })
+  console.log(allPairs)
+  return allPairs
+
+}
+
+  /*for (var i = 0; i < pics_v.length; i=i+2){
     if (pics_v.length - i >= 2){
       allPairs.push(combinePics(pics_v[i],pics_v[i+1]))
     }
-  }
+  }*/
 
-  return allPairs;
-}
+
+  /*var currentIndex = 0
+  pics_v_copy = pics_v
+  while(currentIndex < pics_v_copy.length){
+    var checkForCommons = 0
+    for (var i = ; i < pics_v_copy.length; i++){
+      console.log("currentIndex: ", currentIndex)
+      console.log("i: ",i)
+      console.log("chekcfor: ",checkForCommons)
+      if(pics_v_copy[i] === pics_v_copy[currentIndex]){
+        continue
+      }
+      if(howManyTagsInCommon(pics_v_copy[currentIndex],pics_v_copy[i]) === checkForCommons){
+        allPairs.push(combinePics(pics_v_copy[currentIndex],pics_v_copy[i]))
+        currentIndex++
+      }
+
+    }
+    checkForCommons++
+  }*/
+
+
+
+    /*if(pics_v.length/2 <= allPairs.length){
+      break
+    }
+    if(pics_v.length-i < 2){
+      checkForCommons++
+      i=0
+    }
+    for (var j = 0; j >= pics_v.length-2; j=j++){
+      if(howManyTagsInCommon(pics_v[i],pics_v[j]) == checkForCommons){
+        allPairs.push(combinePics(pics_v[i],pics_v[j]))
+      }
+      if(pics_v.length-j < 2){
+        break;
+      }
+    }*/
+
+
 
 const createUltimatePicArray = (allPairs) => {
   var ultimatePicArray = []
@@ -73,6 +122,32 @@ const createUltimatePicArray = (allPairs) => {
 
 
 
+const howManyTagsInCommon = (pic1, pic2) => {
+  /*var pic1isV = false
+  var pic2isV = false
+
+  if(contains(pics_v,pic1)){
+    pic1isV = true
+  }
+
+  if(contains(pics_v,pic2)){
+    pic2isV = true
+  }*/
+  var numberOfEqualTags = 0
+  var tags1 = pic1["tags"]
+  var tags2 = pic2["tags"]
+
+  for(var i=0; i<tags1.length; i++){
+    for(var j=0; j<tags2.length; j++){
+      if (tags1[i] === tags2[j]){
+        numberOfEqualTags++
+      }
+    }
+  }
+
+  return numberOfEqualTags
+
+}
 
 
 let allPairs = pairAllPicsV();
