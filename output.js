8b58237fcae75ceea99e3ddb1ed54ca54890bc8b
Anton Begehr <a.begehr@gmail.com>
2019-02-28 19:20:07 +0100

    add function write_slides in module.exports

--- output.js
+++ output.js
@@ -4,33 +4,45 @@
 
 const fs = require('fs');
 
-const filename = "out.txt";
 
-var slides = [
+function write_slides(filename, slides) {
+  var slides_text = slides.length + "\n";
+
+  slides.forEach(slide => {
+    // detect wrong slides (not 1 or 2 pictures per slide)
+    if (slide.length !== 1 && slide.length !== 2) {
+      console.error("ERROR: wrong slide: ", slide);
+    }
+
+    slides_text += slide.join(" ") + "\n"
+  });
+
+  console.log("slides_text: ", slides_text);
+
+  fs.writeFile(filename, slides_text, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+
+      console.log("The file was saved!");
+  });
+}
+
+
+// example
+/*
+const ex_filename = "out.txt";
+var ex_slides = [
   [1],
   [2, 3],
   [4, 5],
   [6],
-  [7, 8, 9]
+  [7, 8]
 ];
+write_slides(ex_filename, ex_slides);
+*/
 
-var slides_text = slides.length + "\n";
 
-slides.forEach(slide => {
-  // detect wrong slides (not 1 or 2 pictures per slide)
-  if (slide.length !== 1 && slide.length !== 2) {
-    console.log("ERROR: wrong slide: ", slide);
-  }
-
-  slides_text += slide.join(" ") + "\n"
-});
-
-console.log("slides_text: ", slides_text);
-
-fs.writeFile(filename, slides_text, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-
-    console.log("The file was saved!");
-});
+module.exports = {
+  write_slides
+}
